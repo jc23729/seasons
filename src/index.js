a36b5001da3b763a9922104f8525e3805283fa43
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-21 19:57:42 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,19 @@ class App extends React.Component {
   //constructor function, its the first function anytime an instance of a class is created
   //React says we have to define render//
   constructor(props) {
-      super(props);
-      
-      this.state = { lat: null }; //lat:null just in here we dont know the latitude yet
-  }
-  render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+    super(props);
+
+    this.state = { lat: null }; //lat:null just in here we dont know the latitude yet
+
+      window.navigator.geolocation.getCurrentPosition(
+          (position) => {
+              this.setState({ lat: position.coords.latitude });
+          },
       (err) => console.log(err)
     );
-
-    return <div>Latitude: </div>;
+  }
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
