38cc4ef0d983c55d17c04c68ab2fee71260dadae
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-21 20:21:58 -0400

    Update index.js
    update error message

--- src/index.js
+++ src/index.js
@@ -9,19 +9,22 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null, eorrorMessage: ''};
+    this.state = { lat: null, errorMessage: ''};
 
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         this.setState({ lat: position.coords.latitude });
       },
         (err) => {
-          this.setState({ eorrorMessage: err.message });
+          this.setState({ errorMessage: err.message });
       }
     );
   }
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+      return <div>Latitude: {this.state.lat}
+          <br />
+          Error message: {this.state.errorMessage}
+      </div>;
   }
 }
 
