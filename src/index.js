d862a17a6f277ef6c685e3c2c11ddcc149a44460
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-22 19:06:58 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,25 @@ import ReactDOM from "react-dom";
 if (module.hot) {
   module.hot.accept();
 }
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: "" };
 
-    this.state = { lat: null, errorMessage: "" };
-   
-   
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
   }
+
   //React says we have to define a render
   render() {
-      if (this.state.errorMessage && !this.state.lat) {
-          return <div>Error : {this.state.errorMessage}</div>;
-      }
-      if (!this.state.errorMessage && this.state.lat) {
-          return <div>Latitude: {this.state.lat}</div>;
-      }
-      return <div>Loading...</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
