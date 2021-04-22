9b321a0229f791c15dc0198b28b2b0a774eacaa5
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-04-21 20:01:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,17 +6,15 @@ if (module.hot) {
 }
 
 class App extends React.Component {
-  //constructor function, its the first function anytime an instance of a class is created
-  //React says we have to define render//
   constructor(props) {
     super(props);
 
-    this.state = { lat: null }; //lat:null just in here we dont know the latitude yet
+    this.state = { lat: null };
 
-      window.navigator.geolocation.getCurrentPosition(
-          (position) => {
-              this.setState({ lat: position.coords.latitude });
-          },
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err)
     );
   }
@@ -26,3 +24,26 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// class App extends React.Component {
+//   //constructor function, its the first function anytime an instance of a class is created
+//   //React says we have to define render//
+//   constructor(props) {
+//     super(props);
+//     //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE
+//     this.state = { lat: null }; //lat:null just in here we dont know the latitude yet
+
+//     window.navigator.geolocation.getCurrentPosition(
+//       (position) => {
+//         //we called setState (anytime you want to update your state
+//         this.setState({ lat: position.coords.latitude });
+//       },
+//       (err) => console.log(err)
+//     );
+//   }
+//   render() {
+//     return <div>Latitude: {this.state.lat}</div>;
+//   }
+// }
+
+// ReactDOM.render(<App />, document.querySelector("#root"));
